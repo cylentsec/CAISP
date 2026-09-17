50f621442ecf04aa33f4e9b8825a447857c3aa78
CylentSec <scampbell@cylentsec.com>
2026-09-17 07:05:14 -0400

    Update install-elixir.sh

--- install-elixir.sh
+++ install-elixir.sh
@@ -5,10 +5,27 @@
 apt-get update
 apt-get install -y locales
 
-# Generate the specific UTF-8 locale
-locale-gen en_US.UTF-8
-# Set the locale system-wide in /etc/default/locale
-update-locale LANG=en_US.UTF-8
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+# 1. Update and install locales without sudo
+apt-get update
+apt-get install -y locales
+
+# 2. Uncomment en_US.UTF-8 in the configuration file
+sed -i '/en_US.UTF-8/s/^# //g' /etc/locale.gen
+
+# 3. Generate the uncommented locale
+locale-gen
+
+# 4. Set the system-wide locale (applies to standard users/services)
+update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8
+
+# 5. Force the locale for the root user's environment
+echo 'export LANG=en_US.UTF-8' >> /root/.bashrc
+echo 'export LC_ALL=en_US.UTF-8' >> /root/.bashrc
+source /root/.bashrc
 
 # Install and configure Elixir
 curl -fsSO https://elixir-lang.org/install.sh
